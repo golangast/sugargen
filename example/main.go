6da7f0ee78aa728dc387256476e94c796d77d9e1
zach endrulat <golangast@gmail.com>
2024-05-09 22:11:16 -0500

    fixed main.go

--- example/main.go
+++ example/main.go
@@ -4,52 +4,56 @@ import (
 	"fmt"
 	"slices"
 
+	"github.com/golangast/sugargen/bash"
 	"github.com/golangast/sugargen/chat"
+	"github.com/golangast/sugargen/filefolder"
+	"github.com/golangast/sugargen/gen"
 	"github.com/golangast/sugargen/input"
+	"github.com/golangast/sugargen/text"
 )
 
 func main() {
 	// Parse command line input for a question
-	// answer := input.InputScanDirections("what is your name?")
-	// fmt.Println(answer, " nice name")
+	answer := input.InputScanDirections("what is your name?")
+	fmt.Println(answer, " nice name")
 
-	// // show a list of selection and chosen one is stored
-	// var colorsslice = []string{"red", "green", "blue", "yellow"}
+	// show a list of selection and chosen one is stored
+	var colorsslice = []string{"red", "green", "blue", "yellow"}
 
-	// //remember you can set the number from 1 to whatever you want.  1 is the number of columns
-	// //to add colors update the colors/colors.go file and use https://en.wikipedia.org/wiki/ANSI_escape_code#colors
-	// coloranswer := input.MenuInstuctions(colorsslice, 1, "purple", "purple", "which color do you prefer to use?")
-	// fmt.Println(coloranswer, " is the right answer")
+	//remember you can set the number from 1 to whatever you want.  1 is the number of columns
+	//to add colors update the colors/colors.go file and use https://en.wikipedia.org/wiki/ANSI_escape_code#colors
+	coloranswer := input.MenuInstuctions(colorsslice, 1, "purple", "purple", "which color do you prefer to use?")
+	fmt.Println(coloranswer, " is the right answer")
 
-	// //what can you do with all this? Plenty!
-	// //how about lets make a folder file from the answers
-	// if err := filefolder.Makefolder(answer); err != nil {
-	// 	text.Checklogger(err, "trying to create folder")
-	// }
+	//what can you do with all this? Plenty!
+	//how about lets make a folder file from the answers
+	if err := filefolder.Makefolder(answer); err != nil {
+		text.Checklogger(err, "trying to create folder")
+	}
 
-	// file, err := filefolder.Makefile(answer + "/" + coloranswer + ".txt")
-	// text.Checklogger(err, "trying to add text to file")
+	file, err := filefolder.Makefile(answer + "/" + coloranswer + ".txt")
+	text.Checklogger(err, "trying to add text to file")
 
-	// //you wanna update text in it? Sure we can do that.
-	// //we need some text first.
-	// var sometext = `This is some text but lets add a template map {{.name}}`
+	//you wanna update text in it? Sure we can do that.
+	//we need some text first.
+	var sometext = `This is some text but lets add a template map {{.name}}`
 
-	// //create a map
-	// mb := make(map[string]string)
-	// mb["name"] = answer
+	//create a map
+	mb := make(map[string]string)
+	mb["name"] = answer
 
-	// //now lets do some generating
-	// if err := gen.Writetemplate(sometext, file, mb); err != nil {
-	// 	text.Checklogger(err, "trying to create folder")
-	// }
+	//now lets do some generating
+	if err := gen.Writetemplate(sometext, file, mb); err != nil {
+		text.Checklogger(err, "trying to create folder")
+	}
 
-	// //now lets try to update the text "template map" in the file to "template cat".
-	// text.UpdateTexts(answer+"/"+coloranswer+".txt", "template map", "template map", "template cat")
+	//now lets try to update the text "template map" in the file to "template cat".
+	text.UpdateTexts(answer+"/"+coloranswer+".txt", "template map", "template map", "template cat")
 
-	// //now lets spit out whats in the file using bach in go.
-	// if err := bash.ShellBash("cat ./" + answer + "/" + coloranswer + ".txt"); err != nil {
-	// 	text.Checklogger(err, "trying to do bash in go")
-	// }
+	//now lets spit out whats in the file using bach in go.
+	if err := bash.ShellBash("cat ./" + answer + "/" + coloranswer + ".txt"); err != nil {
+		text.Checklogger(err, "trying to do bash in go")
+	}
 
 	//start training the model! specify the model name and the filename
 	chat.CheckIfSpanLimitsEqualText("server", "server.json")
